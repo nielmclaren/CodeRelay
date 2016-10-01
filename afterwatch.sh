a8a3e6a05f96382a53b19d153dd637d9a9cf91e1
Niel McLaren <d@nielmclaren.com>
2016-09-30 20:42:02 -0700

    Only update the preso if it actually compiles.

--- afterwatch.sh
+++ afterwatch.sh
@@ -3,7 +3,12 @@
 #set -x
 
 sketch_name="TestSketch"
+test_cmd="processing-java --sketch=`pwd`/$sketch_name --build"
 start_cmd="processing-java --sketch=`pwd`/$sketch_name --present"
+
+if [[ $(eval "$test_cmd") != *Finished* ]]; then
+  exit
+fi
 
 # Get process IDs for previous sketch.
 sketch_pids=`ps | grep -i "processing.* --sketch.*/$sketch_name" | grep -v grep | awk '
